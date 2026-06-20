a3a5326f1159e4e92cf15f18fedb245cb3a51125
jasonb322 <bahrcreative@gmail.com>
2026-06-20 10:42:03 -0400

    Update WindData.js, redisplay on homepage

--- src/components/WindData.js
+++ src/components/WindData.js
@@ -1,57 +1,53 @@
-// import { useState, useEffect } from 'react'
-// import { openWeatherMapAPIKey } from './apiKeys'
-// import getData from './getData';
+import { useState, useEffect } from 'react'
+import getData from './getData';
 
 
-// const WindData = () => {
-//     const [windDir, setWindDir] = useState(0);
-//     const [windSpeed, setWindSpeed] = useState(null);
+const WindData = () => {
+    const [windDir, setWindDir] = useState(0);
+    const [windSpeed, setWindSpeed] = useState(null);
 
-//     // .length of windDirArray = 17
-//     const windDirArray = ["N","NNE","NE","ENE","E","ESE","SE","SSE","S","SSW","SW","WSW","W","WNW","NW","NNW","N"]
-    
+    // .length of windDirArray = 17
+    const windDirArray = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW", "N"]
 
-//     const displayWindDirAsString = (rawWindDir) => {
-//         let windAsString = ''
-//         const windSectorIndex = Math.round(rawWindDir/22.5)
-//         if (rawWindDir < 1) {
-//             windAsString = 'N'
-//         } else {
-//             windAsString = windDirArray[windSectorIndex]
-//         }
-//         return windAsString
-//     }
 
-//     const fetchWindData = async () => {
+    const displayWindDirAsString = (rawWindDir) => {
+        let windAsString = ''
+        const windSectorIndex = Math.round(rawWindDir / 22.5)
+        if (rawWindDir < 1) {
+            windAsString = 'N'
+        } else {
+            windAsString = windDirArray[windSectorIndex]
+        }
+        return windAsString
+    }
 
-//     const windURL = 
-//       'https://api.open-meteo.com/v1/forecast?latitude=39.35&longitude=-74.30&hourly=windspeed_10m,winddirection_10m,windgusts_10m&timezone=America/New_York'
+    const fetchWindData = async () => {
 
-//     const data = await getData(windURL)
+        const windURL =
+            'https://api.open-meteo.com/v1/forecast?latitude=39.35&longitude=-74.30&hourly=windspeed_10m,winddirection_10m,windgusts_10m&timezone=America/New_York'
 
-//     // first hourly forecast point
-//     const currentWindDir = data.hourly.winddirection_10m[0]
-//     const currentWindSpeed = data.hourly.windspeed_10m[0]
+        const data = await getData(windURL)
 
-//     console.log(currentWindDir)
-//     console.log(currentWindSpeed)
+        // first hourly forecast point
+        const currentWindDir = data.hourly.winddirection_10m[0]
+        const currentWindSpeed = data.hourly.windspeed_10m[0]
 
-//     setWindDir(currentWindDir)
-//     setWindSpeed(Math.round(currentWindSpeed))
+        setWindDir(currentWindDir)
+        setWindSpeed(Math.round(currentWindSpeed))
 
-//     return data
-// }
+        return data
+    }
 
-//     useEffect(() => {
-//         fetchWindData().catch((err) => console.log(err))
-//     }, [])
+    useEffect(() => {
+        fetchWindData().catch((err) => console.log(err))
+    }, [])
 
-//     return (
-//         <div>
-//             <h4>Wind:</h4>
-//             <h4>{ (windDir!=null && windSpeed!=null) ? `${displayWindDirAsString(windDir)} @ ${windSpeed} mph` : 'Man, wind still broken :(' }</h4>
-//         </div>
-//     )
-// }
+    return (
+        <div>
+            <h4>Wind:</h4>
+            <h4>{(windDir != null && windSpeed != null) ? `${displayWindDirAsString(windDir)} @ ${windSpeed} mph` : 'Man, wind still broken :('}</h4>
+        </div>
+    )
+}
 
-// export default WindData
+export default WindData
