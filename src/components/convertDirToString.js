11ea460203e008dd149dfa833ae0ea2556fd8e36
jasonb322 <bahrcreative@gmail.com>
2026-06-20 14:02:03 -0400

    create convertAngleToString exported method

---
+++ src/components/convertDirToString.js
@@ -0,0 +1,11 @@
+export const convertAngleToString = (angle) => {
+	const compassArray = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW", "N"]
+	let angleToString = ''
+	const sectorIndex = Math.round(rawWindDir / 22.5)
+	if (rawWindDir < 1) {
+		angleToString = 'N'
+	} else {
+		angleToString = compassArray[sectorIndex]
+	}
+	return angleToString
+}
