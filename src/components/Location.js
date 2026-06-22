be6cf5bcb633cf6a68b616332d3dc802ca9f8727
jasonb322 <bahrcreative@gmail.com>
2026-06-22 15:30:03 -0400

    update live conditions display Location.js

--- src/components/Location.js
+++ src/components/Location.js
@@ -29,7 +29,7 @@ const Location = () => {
     <div className='location'>
       <h1>{name}</h1>
       <h2>Tides and Temps</h2>
-      <h3>{"Updated: " + localTime}</h3>
+      <h3>{"Live Conditions:\n " + localTime}</h3>
       {/* <div className='locationStats'>
         <h4>Lat: {lat}</h4>
         <h4>Long: {long}</h4>
